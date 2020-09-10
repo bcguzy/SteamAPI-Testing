6d2722dc426e7ad99ba26e2cd912ea6b9400f54c
Brandon G <bcguzy@gmail.com>
2020-09-09 21:08:34 -0400

    updated key to be for general bcguzy.github.io

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 
 //document.getElementById("change").innerHTML = "Hello World";
 function getPlayerInfo(){
-    var key = '43B2EAC6404A2AADBB1A066159365544'; //openweathermap key
-    fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=21AD387946B549C0C2F0A5AEE0E0A308&steamids=76561197960435530')
+    fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=1FBE4A2A938B56E4B696B13F88A04DBF&steamids=76561197960435530')
     .then(response => response.json())
     .then(data => console.log(data));
 }
