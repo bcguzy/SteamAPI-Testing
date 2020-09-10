3efe31c8dcd4a59febf9e9df44553b365a6e6151
Brandon G <bcguzy@gmail.com>
2020-09-09 21:14:58 -0400

    added comma

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 //document.getElementById("change").innerHTML = "Hello World";
 function getPlayerInfo(){
     fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=1FBE4A2A938B56E4B696B13F88A04DBF&steamids=76561197960435530',{
-        mode: 'cors'
+        mode: 'cors',
         headers:{
             'Access-Control-Allow-Origin': '*'
         }
