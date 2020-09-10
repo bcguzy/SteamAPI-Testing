2b11f43f5fa23cddd24339c668ca58ced1ce5237
Brandon G <bcguzy@gmail.com>
2020-09-09 21:02:08 -0400

    updated API key

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 //document.getElementById("change").innerHTML = "Hello World";
 function getPlayerInfo(){
-    var key = 'F5712EEA8ED9B25B21216D391A90BA41'; //openweathermap key
+    var key = '43B2EAC6404A2AADBB1A066159365544'; //openweathermap key
     fetch('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=21AD387946B549C0C2F0A5AEE0E0A308&steamids=76561197960435530')
     .then(response => response.json())
     .then(data => console.log(data));
