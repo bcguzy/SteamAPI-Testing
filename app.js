1872affd6a36d23f513ae56c2649018c45586cdf
Brandon G <bcguzy@gmail.com>
2020-09-09 21:12:48 -0400

    attempting basic cors implementation

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 'use strict';
-
+  
 //document.getElementById("change").innerHTML = "Hello World";
 function getPlayerInfo(){
-    fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=1FBE4A2A938B56E4B696B13F88A04DBF&steamids=76561197960435530')
+    fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=1FBE4A2A938B56E4B696B13F88A04DBF&steamids=76561197960435530',{
+        mode: 'cors'
+        headers:{
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    )
     .then(response => response.json())
     .then(data => console.log(data));
 }
